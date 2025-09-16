a770d777d0c3e16293c53e64f4a5b4107649c6cd
Masseh Ziaie <mas154572@gmail.com>
2025-09-15 23:48:23 -0400

    setting things up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Calendar } from "./Calendar";
+import "./index.css";
+export default function App() {
+  return (
+    <>
+      <main>
+        <Calendar />
+      </main>
+    </>
+  );
+}
---
+++ src/Calendar.tsx
@@ -0,0 +1,7 @@
+export function Calendar() {
+  return (
+    <div className="bg-neutral-50 w-full h-screen">
+      <h1>Date</h1>
+    </div>
+  );
+}
