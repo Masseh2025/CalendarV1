67414a1240cd2c3ef120d341e1212bd05ee55538
Masseh Ziaie <mas154572@gmail.com>
2025-09-24 04:42:04 -0400

    added icon library/styled heading/added up and down

--- src/CalendarHeader.tsx
+++ src/CalendarHeader.tsx
@@ -1,11 +1,24 @@
+import { ChevronUpCircle, ChevronDownCircle } from "lucide-react";
+
 type CalendarHeaderType = {
   month: number;
 };
 
 export function CalendarHeader({ month }: CalendarHeaderType) {
   return (
-    <div className="h-[15vh]">
-      <h1>Today is {monthNames[month]}</h1>
+    <div className="h-[15vh] flex justify-center items-baseline">
+      <ChevronUpCircle
+        size={24}
+        className="text-neutral-600 hover:text-blue-500 cursor-pointer"
+      />
+
+      <h1 className="text-4xl font-bold text-neutral-800 p-4 text-center">
+        {monthNames[month]}
+      </h1>
+      <ChevronDownCircle
+        size={24}
+        className="text-neutral-600 hover:text-blue-500 cursor-pointer"
+      />
     </div>
   );
 }
